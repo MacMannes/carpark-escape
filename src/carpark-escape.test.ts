fb49c4001ed43429c7b5cd2fc8db9170d43e985a
André Mathlener <andre.mathlener@ns.nl>
2024-11-28 11:56:42 +0100

    Got started on writing the tests

--- src/carpark-escape.test.ts
+++ src/carpark-escape.test.ts
@@ -1,9 +1,19 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { CarParkEscape } from './carpark-escape';
 
-describe('CarPark Escape', () => {
-    it('Creating an instance of the CarParkEscape class should work', () => {
-        const clazz = new CarParkEscape();
-        expect(clazz).toBeDefined();
+describe('CarPark Escape Acceptance', () => {
+    test('Escape carpark from the first floor', () => {
+        const carpark = new CarParkEscape([
+            [1, 0, 0, 0, 2],
+            [0, 0, 0, 0, 0],
+        ]);
+        expect(carpark.escape()).toEqual(['L4', 'D1', 'R4']);
+    });
+});
+
+describe('CarPark Escape should', () => {
+    test('Should escape when 1 floor only', () => {
+        const carpark = new CarParkEscape([[0, 0, 2, 0, 0]]);
+        expect(carpark.escape()).toEqual(['R2']);
     });
 });
